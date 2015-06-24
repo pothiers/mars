694fb72a0b72b77186a416514c90f7a4180aa635
Steve Pothier <pothier@noao.edu>
2015-06-24 05:28:33 -0700

    changed project name

--- marssite/manage.py
+++ marssite/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nasite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marssite.settings")
 
     from django.core.management import execute_from_command_line
 
