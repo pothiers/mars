4f105657170bd1df246ec3f5ea7fe2311da1b48f
Steve Pothier <pothiers@gmail.com>
2015-06-25 08:32:09 -0700

    first release shown to Sean

--- marssite/siap/urls.py
+++ marssite/siap/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     # eg: /siap/
     url(r'^$', views.index, name='index'),
-    # eg: /siap/cp243352.fits.gz
-    url(r'^(?P<propid>.+)/$', views.filenames, name='filenames'),
+
+    # eg: /siap/prop/2011A-0525
+    url(r'^prop/(?P<propid>.+)/$', views.filenames, name='filenames'),
+    # eg: /siap/detail/cp243352.fits.gz
+    url(r'^detail/(?P<image_id>.+)/$', views.detail, name='detail'),
 ]
--- marssite/siap/views.py
+++ marssite/siap/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Image
 
 # Create your views here.
 
 def index(request):
-    im_list = Image.objects.raw('SELECT * FROM voi.siap LIMIT 5') #!!! limit 5
-    output = '<br />'.join([im.reference for im in im_list])
-    return HttpResponse(output)
+    im_list = Image.objects.raw('SELECT * FROM voi.siap LIMIT 25') #!!! not all
+    template = loader.get_template('siap/index.html')
+    context = RequestContext(request, {
+        'recent_image_list': im_list,
+    })
+    #!output = '<br />'.join([im.reference for im in im_list])
+    return HttpResponse(template.render(context))
 
 def filenames(request, propid):
     output = '<table border="1"> '
-    for im in Image.objects.raw("SELECT * FROM voi.siap WHERE prop_id = '{}' LIMIT 5".format(propid)): #!!! limit 5
+    for im in Image.objects.raw("SELECT * FROM voi.siap WHERE prop_id = '{}' LIMIT 25".format(propid)): #!!! limit 
         output += '<tr><td>{}</td><td>{}</td><td>{}</td></tr>'.format(im.dtpropid, im.dtnsanam, im.dtacqnam)
-    return HttpResponse("Details for image: %s<p>%s" % (propid,output))
-                        
+    return HttpResponse("Files for prop_id: %s<p>%s" % (propid,output))
 
+def detail(request, image_id):
+    im_list = Image.objects.raw("SELECT * FROM voi.siap WHERE reference = '{}'".format(image_id))
+    template = loader.get_template('siap/detail.html')
+    context = RequestContext(request, {
+        'dict': im_list[0].__dict__,
+    })
+
+    #return HttpResponse("Details for image: %s<p>%s" % (image_id,im_list[0].__dict__))
+    return HttpResponse(template.render(context))
