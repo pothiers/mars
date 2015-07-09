11dd3edb0a6adf0e8df13b75bcaaed395e9717f0
Steve Pothier <pothier@noao.edu>
2015-07-09 13:42:38 -0700

    snap

--- marssite/siap/views.py
+++ marssite/siap/views.py
@@ -7,21 +7,24 @@ from .models import Image
 
 def index(request):
     context = RequestContext(request, {
-        'recent_image_list': Image.objects.raw('SELECT * FROM voi.siap LIMIT 25') #!!! not all
+        'recent_image_list': Image.objects
+        .raw('SELECT * FROM voi.siap LIMIT 25') #!!! not all
     })
     return render(request, 'siap/index.html', context)
 
 def filenames(request, propid):
     context = RequestContext(request, {
         'propid': propid,
-        'image_list': Image.objects.raw("SELECT * FROM voi.siap  WHERE prop_id = %s",[propid])
+        'image_list': Image.objects
+        .raw("SELECT * FROM voi.siap  WHERE prop_id = %s",[propid])
     })
     
     return render(request, 'siap/filenames.html', context)
 
 def detail(request, image_id):
     #!im = get_object_or_404(Image, pk=image_id)
-    im_list = Image.objects.raw("SELECT * FROM voi.siap WHERE reference = %s", [image_id])
+    im_list = (Image.objects
+               .raw("SELECT * FROM voi.siap WHERE reference = %s", [image_id]))
     context = RequestContext(request, {
         #!'dict': im.__dict__,
         'dict': im_list[0].__dict__,
