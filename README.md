a91f57423947d5e82160fd3355418a11c1cf7c06
Steve Pothier <pothiers@gmail.com>
2015-09-28 07:45:56 -0700

    snap

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # mars
 Metadata Archive Retrival Services
-i
--- marssite/schedule/urls.py
+++ marssite/schedule/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # The whole schedule
     url(r'^$', views.list, name='list'),
 
-    # eg: /schedule/prop/ct13m/2014-12-25  => smarts
+    # eg: /schedule/prop/ct13m/2014-12-25/  => smarts
     url(r'^prop/(?P<tele>.+)/(?P<date>.+)/$',
         views.schedprop, name='schedprop'),
 ]
