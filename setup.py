45f40ebf563a93ecdf158feca63ba6868fec0037
Steve Pothier <pothiers@gmail.com>
2016-07-15 10:12:02 -0700

    snap

--- setup.py
+++ setup.py
@@ -84,7 +84,8 @@ setup(
     #!package_data={
     #!    'tada_support': ['personalities/*', 'scripts/*', 'dev-scripts/*',],
     #!},
-    package_data={'marssite': ['water/VERSION','water/UPDATED', 'static/*', 'static/*/*', '*/static/*']},
+    package_data={'marssite': ['water/VERSION','water/UPDATED',
+                               'static/*', 'static/*/*', '*/static/*/*']},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages.
