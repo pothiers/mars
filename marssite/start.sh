c7e2dddd913330fe146d1cf16f99585e884aa855
Steve Pothier <pothiers@gmail.com>
2016-08-01 06:50:43 -0700

    snap

--- marssite/start.sh
+++ marssite/start.sh
@@ -9,3 +9,7 @@ nohup unbuffer python3 -u manage.py runserver 0.0.0.0:8000 >  $LOG &
 # gunicorn --bind 0.0.0.0:8000 marssite.wsgi:application
 # gunicorn --pythonpath /usr/lib/python3.4/site-packages/marssite --bind 0.0.0.0:8000 marssite.wsgi:application
 tail -F $LOG &
+
+
+# To find where mars is installed:
+#   python3 -m inspect -d marssite
