347ec0d515b280280ef4bed24401ad4f7ff416f2
Steve Pothier <pothiers@gmail.com>
2016-08-17 21:49:17 -0700

    snap

--- marssite/prod-start.sh
+++ marssite/prod-start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 LOG=/var/log/mars/mars.log
 cd /opt/mars/marssite
-gunicorn --bind 0.0.0.0:8000 marssite.wsgi:application >> $LOG &
+gunicorn --env DJANGO_SETTINGS_MODULE=marssite.settings \
+	 --bind 0.0.0.0:8000 \
+	 marssite.wsgi:application >> $LOG &
 
