6e3152a9632b4c78dfbbee170049f48adbbf4854
Steve Pothier <pothier@noao.edu>
2017-01-23 09:17:00 -0700

    code comments

--- marssite/gunicorn_start.sh
+++ marssite/gunicorn_start.sh
@@ -3,6 +3,7 @@
 
 
 #! python manage.py collectstatic
+# then browse to http://localhost:8000/static/images/sitelogo.png
 
 source /opt/mars/venv/bin/activate
 gunicorn --pythonpath /opt/mars/marssite  --bind 0.0.0.0:8000 marssite.wsgi:application
