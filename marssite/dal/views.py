3af98972ba3b5d0e37ec72ad85c62588fbcf5e96
Steve Pothier <pothier@noao.edu>
2017-05-22 14:01:53 -0700

    converts JSON to XML naively

--- marssite/dal/views.py
+++ marssite/dal/views.py
@@ -1,5 +1,6 @@
-
 import json
+import xml.etree.ElementTree as ET
+
 
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -70,10 +71,26 @@ def search_by_json(request, limit=None):
     print('EXECUTING: views<dal>:search_by_file; method={}, content_type={}'
           .format(request.method, request.content_type))
     if request.method == 'POST':
+        root = ET.Element('search')
         print('body str={}'.format(request.body.decode('utf-8')))
-        body = json.loads(request.body.decode('utf-8'))
-        print('body={}'.format(body))
-        jsearch = body['search']
+        if request.content_type == "application/json":
+            body = json.loads(request.body.decode('utf-8'))
+            #print('body={}'.format(body))
+            jsearch = body['search']
+            print('jsearch={}'.format(jsearch))
+            for k,v in jsearch.items():
+                e = ET.SubElement(root, k)
+                if isinstance(v, dict):
+                    for k2,v2 in v.items():                 
+                        ET.SubElement(e, k2).text = str(v2)
+                else:
+                    e.text = str(v)
+            print('root={}'.format(root))
+            tree = ET.ElementTree(root)
+            xmlstr = ET.tostring(root)
+            print('xml search={}'.format(xmlstr))
+        elif request.content_type == "application/xml":
+            pass
 
         #return JsonResponse(serializers.serialize(format, qs), safe=False)
         return JsonResponse(body, safe=False)
