f071bc1a50315e97cc74816810e400ec212a43a6
Peter Peterson <ppeterson@noao.edu>
2017-07-10 08:10:25 -0700

    cleaning up static resources, now bundled into app specific files using webpack

--- marssite/static/natica/static/spec/testSearch.js
+++
@@ -1,8 +0,0 @@
-describe("It should conduct a search", function() {
-  it("Does some simple addition", function() {
-    return expect(3 + 2).toEqual(5);
-  });
-  return it("Fails when adding a char + num", function() {
-    return expect(3 + 'a').toEqual('3a');
-  });
-});
